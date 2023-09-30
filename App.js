9b3cddd1361918bfaf9cafd9eeeb6ecf1279bfd6
Luigi-Recco <143564200+Luigi-Recco@users.noreply.github.com>
2023-09-29 22:50:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import  {NavigationContainer} from '@react-navigation/native';
+
+import Rotas from './Pages/Rotas';
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+    <Rotas/>
+    </NavigationContainer>
+  )
+}
---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-_3bRJEiQNDz1DzQpjsQinGBv7k-51Uc",
+  authDomain: "pizzariarataobd.firebaseapp.com",
+  projectId: "pizzariarataobd",
+  storageBucket: "pizzariarataobd.appspot.com",
+  messagingSenderId: "664569008331",
+  appId: "1:664569008331:web:62acf27addf6bfce164b4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
